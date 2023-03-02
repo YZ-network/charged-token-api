0957655311ec81dda1a90cd92f9b8210d7c8e44d
Youssouf Naciri <ynaciri@gmail.com>
2023-03-02 09:36:55 +0100

    restored contracts index

---
+++ src/contracts/index.ts
@@ -0,0 +1,11 @@
+import ContractsDirectory from "./ContractsDirectory.json";
+import DelegableToLT from "./DelegableToLT.json";
+import InterfaceProjectToken from "./InterfaceProjectToken.json";
+import LiquidityToken from "./LiquidityToken.json";
+
+export const contracts = {
+  ContractsDirectory,
+  LiquidityToken,
+  DelegableToLT,
+  InterfaceProjectToken,
+};
