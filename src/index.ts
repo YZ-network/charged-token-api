3c89c983b3401e879e65e79f07e527fbae70b7d9
Youssouf Naciri <ynaciri@gmail.com>
2023-04-19 23:06:25 +0200

    removed excess logging

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import { rootLogger } from "./util";
 const log = rootLogger.child({ name: "index" });
 
 Main.init();
-Main.start()
-  .then(() => log.info("GraphQL API stopped"))
-  .catch((err) => log.error({ msg: "GraphQL API stopped with error", err }));
+Main.start().catch((err) =>
+  log.error({ msg: "GraphQL API stopped with error", err })
+);
