0101ba3243866ee85a294048d0839869dc8fd89f
Youssouf Naciri <ynaciri@gmail.com>
2023-04-28 15:26:20 +0200

    added logging for all events topics

--- src/logevents.ts
+++ src/logevents.ts
@@ -1,10 +1,14 @@
 import { ethers } from "ethers";
 import Directory from "./contracts/ContractsDirectory.json";
+import DelegableToLT from "./contracts/DelegableToLT.json";
+import InterfaceProjectToken from "./contracts/InterfaceProjectToken.json";
 import ChargedToken from "./contracts/LiquidityToken.json";
 
 export function dumpEvents() {
   const dirInterface = new ethers.utils.Interface(Directory.abi);
   const ctInterface = new ethers.utils.Interface(ChargedToken.abi);
+  const ifInterface = new ethers.utils.Interface(InterfaceProjectToken.abi);
+  const ptInterface = new ethers.utils.Interface(DelegableToLT.abi);
 
   for (const e of Directory.abi.filter((ev) => ev.type === "event")) {
     console.log(
@@ -19,6 +23,24 @@ export function dumpEvents() {
       "LiquidityToken:",
       e.name,
       ...ctInterface.encodeFilterTopics(e.name!, [])
+    );
+  }
+
+  for (const e of InterfaceProjectToken.abi.filter(
+    (ev) => ev.type === "event"
+  )) {
+    console.log(
+      "InterfaceProjectToken:",
+      e.name,
+      ...ifInterface.encodeFilterTopics(e.name!, [])
+    );
+  }
+
+  for (const e of DelegableToLT.abi.filter((ev) => ev.type === "event")) {
+    console.log(
+      "DelegableToLT:",
+      e.name,
+      ...ptInterface.encodeFilterTopics(e.name!, [])
     );
   }
 }
