b4106ff5c72308edff6db5053c058e9bf8608d98
Youssouf Naciri <ynaciri@gmail.com>
2024-02-06 11:46:01 +0100

    fixed imports bypassing contracts facade

--- src/topics.ts
+++ src/topics.ts
@@ -1,8 +1,5 @@
 import { ethers } from "ethers";
-import Directory from "./contracts/ContractsDirectory.json";
-import DelegableToLT from "./contracts/DelegableToLT.json";
-import InterfaceProjectToken from "./contracts/InterfaceProjectToken.json";
-import ChargedToken from "./contracts/LiquidityToken.json";
+import { contracts } from "./contracts";
 import { rootLogger } from "./rootLogger";
 
 const log = rootLogger.child({ name: "encodeEvents" });
@@ -11,33 +8,31 @@ export function encodeEvents() {
   const topicsMap: Record<string, Record<string, string>> = {
     Directory: {},
     ChargedToken: {},
-    FundraisingChargedToken: {},
     InterfaceProjectToken: {},
     DelegableToLT: {},
   };
 
-  const dirInterface = new ethers.utils.Interface(Directory.abi);
-  const ctInterface = new ethers.utils.Interface(ChargedToken.abi);
-  const ifInterface = new ethers.utils.Interface(InterfaceProjectToken.abi);
-  const ptInterface = new ethers.utils.Interface(DelegableToLT.abi);
+  const dirInterface = new ethers.utils.Interface(contracts.ContractsDirectory.abi);
+  const ctInterface = new ethers.utils.Interface(contracts.LiquidityToken.abi);
+  const ifInterface = new ethers.utils.Interface(contracts.InterfaceProjectToken.abi);
+  const ptInterface = new ethers.utils.Interface(contracts.DelegableToLT.abi);
 
-  for (const e of Directory.abi.filter((ev) => ev.type === "event")) {
+  for (const e of contracts.ContractsDirectory.abi.filter((ev) => ev.type === "event")) {
     const [topic] = dirInterface.encodeFilterTopics(e.name, []) as string[];
     topicsMap.Directory[topic] = e.name!;
   }
 
-  for (const e of ChargedToken.abi.filter((ev) => ev.type === "event")) {
+  for (const e of contracts.LiquidityToken.abi.filter((ev) => ev.type === "event")) {
     const [topic] = ctInterface.encodeFilterTopics(e.name!, []) as string[];
     topicsMap.ChargedToken[topic] = e.name!;
-    topicsMap.FundraisingChargedToken[topic] = e.name!;
   }
 
-  for (const e of InterfaceProjectToken.abi.filter((ev) => ev.type === "event")) {
+  for (const e of contracts.InterfaceProjectToken.abi.filter((ev) => ev.type === "event")) {
     const [topic] = ifInterface.encodeFilterTopics(e.name!, []) as string[];
     topicsMap.InterfaceProjectToken[topic] = e.name!;
   }
 
-  for (const e of DelegableToLT.abi.filter((ev) => ev.type === "event")) {
+  for (const e of contracts.DelegableToLT.abi.filter((ev) => ev.type === "event")) {
     const [topic] = ptInterface.encodeFilterTopics(e.name, []) as string[];
     topicsMap.DelegableToLT[topic] = e.name!;
   }
