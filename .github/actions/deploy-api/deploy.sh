e5f76f3d7e376578ac507c6bd3148f60c0f2fea8
Youssouf Naciri <ynaciri@gmail.com>
2024-03-24 14:37:02 +0100

    wip

--- .github/actions/deploy-api/deploy.sh
+++ .github/actions/deploy-api/deploy.sh
@@ -2,10 +2,10 @@
 
 # Environment to set prior to calling this script :
 #
-# IMAGE_TAG  : release or snapshot
-# CONFIG_VAR : name of the SSM parameter containing the configuration
-# CONTAINER  : name of the container to start
-# LOG_GROUP  : aws log group for the container
+# IMAGE_TAG   : release or snapshot
+# CONFIG_VAR  : name of the SSM parameter containing the configuration
+# CONTAINER   : name of the container to start
+# LOG_GROUP   : aws log group for the container
 #
 
 echo "Authenticating to ECR"
@@ -23,7 +23,7 @@ CONFIG=$(aws ssm get-parameter --no-paginate --name $CONFIG_VAR --query 'Paramet
 echo "Starting container $CONTAINER"
 sudo docker run --log-driver=awslogs --log-opt awslogs-create-group=true --log-opt awslogs-group=${LOG_GROUP} \
   -d --name $CONTAINER --restart always --network db_net \
-  -e "CONFIG=${CONFIG}" \
+  -e "CONFIG=$CONFIG" \
   --health-cmd "curl -f http://localhost/health" \
   --health-interval 30s --health-timeout 10s --health-retries 5 \
   940219468121.dkr.ecr.eu-west-3.amazonaws.com/yz-image:${IMAGE_TAG}
