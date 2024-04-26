e68325e5adff489ee47280476a1b53681d52c31b
Youssouf Naciri <ynaciri@gmail.com>
2024-04-26 18:10:55 +0200

    fixed tests

--- src/api/resolvers/__tests__/health.test.ts
+++ src/api/resolvers/__tests__/health.test.ts
@@ -61,6 +61,6 @@ describe("Health check query resolver", () => {
     expect(await result.return()).toStrictEqual({ value: undefined, done: true });
 
     expect(nextMock).toBeCalledTimes(healthCount);
-    expect(returnMock).toBeCalledTimes(1);
+    expect(broker.unsubscribe).toBeCalled();
   });
 });
