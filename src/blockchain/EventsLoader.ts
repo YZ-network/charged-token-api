03a72de15ba233b33522dbe98a4933e1aea89de6
Youssouf Naciri <ynaciri@gmail.com>
2024-06-14 13:26:54 +0200

    split events loading to send 4 topics in filter at a time

--- src/blockchain/EventsLoader.ts
+++ src/blockchain/EventsLoader.ts
@@ -132,17 +132,20 @@ export class EventsLoader {
 
   private async loadBlockEvents(fromBlock: number, toBlock: number): Promise<string[]> {
     const txHashes = new Set<string>();
-    const knownTopics = new Set(Object.values(topicsMap).flatMap((topics) => Object.keys(topics)));
+    const knownTopics = [...new Set(Object.values(topicsMap).flatMap((topics) => Object.keys(topics)))];
 
-    const eventFilter = {
-      fromBlock,
-      toBlock,
-      topics: [...knownTopics],
-    };
+    this.log.debug({ msg: "Loading events", fromBlock, toBlock });
 
-    this.log.debug({ msg: "Loading events", eventFilter });
+    const events = [];
+    for (let i = 0; i < knownTopics.length; i += 4) {
+      const eventFilter = {
+        fromBlock,
+        toBlock,
+        topics: knownTopics.slice(i, i+4),
+      };
 
-    const events = await this.provider.getLogs(eventFilter);
+      events.push(...(await this.provider.getLogs(eventFilter));
+    }
 
     this.log.debug({ msg: "Found events", count: events.length });
 
@@ -151,7 +154,6 @@ export class EventsLoader {
     this.log.debug({ msg: "On watched contracts", count: knownContractsEvents.length });
 
     const knownEvents = knownContractsEvents
-      .filter((event) => knownTopics.has(event.topics[0]))
       .sort((a, b) => {
         if (a.blockNumber < b.blockNumber) return -1;
         if (a.blockNumber > b.blockNumber) return 1;
